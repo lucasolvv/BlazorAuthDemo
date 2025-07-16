dbe0bff386c4fbb80a30e83058ed5135baf3d054
Lucas <lvksounds@gmail.com>
2025-07-16 16:12:36 -0300

    refactor(auth): 🔒 Remover exibição de login e simplificar autenticação
    * Removido o componente `LoginDisplay` e a lógica de logout.
    * Adicionado link de navegação para a página de login no `NavMenu.razor`.
    * Eliminada a lógica de redirecionamento em `RedirectToLogin.razor`.
    * Removidas referências ao `RemoteAuthenticatorView` em `Authentication.razor`.
    * Excluído o arquivo `Counter.razor`, removendo a funcionalidade de contagem.
    * Adicionada autorização na página `Home.razor`, exigindo autenticação.
    * Modificado `Login.razor` para usar um novo modelo de login e simplificar a lógica.
    * Excluído o arquivo `Weather.razor`, removendo a funcionalidade de previsão do tempo.
    * Adicionados serviços de autenticação e autorização em `Program.cs`, incluindo OIDC.
    * Introduzido `AuthState.cs` para gerenciar o estado de autenticação do usuário.

--- BlazorAuthDemo.Client/Program.cs
+++ BlazorAuthDemo.Client/Program.cs
@@ -1,12 +1,19 @@
+using BlazorAuthDemo.Client;
+using BlazorAuthDemo.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorAuthDemo.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5180") });
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthState>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider => provider.GetRequiredService<AuthState>());
+
 
 builder.Services.AddOidcAuthentication(options =>
 {
---
+++ BlazorAuthDemo.Client/Services/AuthState.cs
@@ -0,0 +1,39 @@
+﻿using BlazorAuthDemo.Shared.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+namespace BlazorAuthDemo.Client.Services
+{
+    public class AuthState : AuthenticationStateProvider
+    {
+        private User? _currentUser;
+
+        public async Task Login(User user)
+        {
+            _currentUser = user;
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public async Task Logout()
+        {
+            _currentUser = null;
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            if (_currentUser != null)
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                new Claim(ClaimTypes.Name, _currentUser.Name),
+                new Claim(ClaimTypes.Email, _currentUser.Email)
+            }, "FakeAuth");
+
+                var user = new ClaimsPrincipal(identity);
+                return Task.FromResult(new AuthenticationState(user));
+            }
+
+            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+        }
+
+    }
+}
